3ac1c4f90671ee12254531e995699b4c146f2524
Justyna <justynatarlaga9404@gmail.com>
2019-03-10 16:40:07 +0100

    add localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {ThoughtsService} from "./thoughts.service";
 
 @Component({
   selector: 'app-root',
@@ -9,12 +10,21 @@ export class AppComponent {
   public text: String = "szczerozłota myśl";
   public thoughts: Array<String> = [];
 
+  constructor(
+    private thoughtsService: ThoughtsService
+  ) {}
+
+  private ngOnInit() {
+    this.thoughts = this.thoughtsService.fetchThoughts();
+  }
+
   public addThought(): void {
     this.thoughts.push(this.text);
     this.text = "";
+    this.thoughtsService.saveThoughts(this.thoughts);
   }
 
   public removeThought(i): void {
-    this.thoughts.splice(i, 1)
+    this.thoughts.splice(i, 1);
   }
 }
